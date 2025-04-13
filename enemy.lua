2a6bb62c072aa1f5d6cae191441badf920b67642
laurent-indermuehle <laurent.indermuehle@pm.me>
2025-04-13 22:37:41 +0200

    Add enemies now fall asleep and player can wake them up

--- enemy.lua
+++ enemy.lua
@@ -1,11 +1,57 @@
-function new_enemy(x, y)
-	local sprite=2
+function new_enemy()
 
-	local enemy = {
-		draw=function(_ENV)
-			spr(sprite, x, y)
-		end,
-	}
 
-	return enemy
+  local enemy = {
+    x = rnd(127),
+    y = rnd(127),
+    sprite = 2,
+    next_asleep = next_time_asleep(),
+    draw = function(_ENV)
+      spr(sprite, x, y)
+    end,
+  }
+
+  return enemy
+end
+
+function sleep(enemy)
+  enemy.sprite = 3
+  
+  if check_collision(enemy) then
+    enemy.sprite = 2  -- Wake up
+    enemy.next_asleep = next_time_asleep()
+  end
+end
+
+function next_time_asleep()
+  return enemy_min_before_asleep + rnd(enemy_max_before_asleep)
+end
+
+function check_collision(enemy)
+  -- Enemy's sprite boundaries (assuming 8x8 sprites)
+  local e_left = enemy.x
+  local e_right = enemy.x + 7
+  local e_top = enemy.y
+  local e_bottom = enemy.y + 7
+  
+  -- Player's four corners
+  local corners = {
+    {player.x, player.y},         -- Top-left
+    {player.x + 7, player.y},     -- Top-right
+    {player.x, player.y + 7},     -- Bottom-left
+    {player.x + 7, player.y + 7}  -- Bottom-right
+  }
+  
+  -- Check if any player corner is inside the enemy sprite
+  for i=1,4 do
+    local corner_x = corners[i][1]
+    local corner_y = corners[i][2]
+    
+    if corner_x >= e_left and corner_x <= e_right and 
+       corner_y >= e_top and corner_y <= e_bottom then
+      return true
+    end
+  end
+  
+  return false
 end
--- main.lua
+++ main.lua
@@ -2,16 +2,15 @@ function _init()
 	log("game initialized")
 
 	--config
-	w=128
-	h=128
-	enemies_count=5
+	w = 128
+	h = 128
+	enemies_count = 5
+	enemy_min_before_asleep = 300
+	enemy_max_before_asleep = 6000
 
-	enemies={}
-	for _=1, enemies_count do
-		enemy = new_enemy(
-			rnd(127),
-			rnd(127)
-		)
+	enemies = {}
+	for _ = 1, enemies_count do
+		enemy = new_enemy()
 
 		add(enemies, enemy)
 	end
@@ -19,6 +18,13 @@ end
 
 function _update60()
 	move(player)
+
+	for enemy in all(enemies) do
+		enemy.next_asleep -= 1
+		if enemy.next_asleep <= 0 then
+			sleep(enemy)
+		end
+	end
 end
 
 function _draw()
