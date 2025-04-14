7c5ffeba6fb25fd39668a8d4ad93f4cf87e57643
laurent-indermuehle <laurent.indermuehle@pm.me>
2025-04-14 10:50:51 +0200

    Add drop food method

--- main.lua
+++ main.lua
@@ -7,6 +7,8 @@ function _init()
 	enemies_count = 5
 	enemy_min_before_asleep = 100
 	enemy_max_before_asleep = 2000
+	drop_food_min_interv = 180
+	foods={}
 
 	enemies = {}
 	for _ = 1, enemies_count do
@@ -17,7 +19,8 @@ function _init()
 end
 
 function _update60()
-	move(player)
+
+	player:update()
 
 	for enemy in all(enemies) do
 		enemy.next_asleep -= 1
@@ -32,5 +35,10 @@ function _draw()
 	player:draw()
 	for enemy in all(enemies) do
 		enemy:draw()
+	end
+
+	for food in all(foods) do
+		--      spr,      x,       y,    width,  height,  flip x,  flip y
+		spr(food[1], food[2], food[3], food[4], food[5], food[6], food[7])
 	end
 end
--- player.lua
+++ player.lua
@@ -1,13 +1,47 @@
 player={
-	x=0,
-	y=0,
-	sprit=0,
-	flipx=false,
-	flipy=false,
+  x=0,
+  y=0,
+  sprit=0,
+  flipx=false,
+  flipy=false,
   colli=true,
   colliword=true,
+  last_drop_food=0,
 
-	draw=function(_ENV)
-		spr(sprit,x,y)
-	end,
+  update=function(self)
+    self.last_drop_food+=1
+
+    move(self)
+
+    if self.last_drop_food < drop_food_min_interv then
+      return
+    end
+
+    log("Food ready")
+
+    -- button X
+    if (btn(5)) then
+      log("X pressed")
+      self:drop_food()
+      self.last_drop_food = 0
+    end
+  end,
+
+  draw=function(self)
+    spr(self.sprit, self.x, self.y)
+  end,
+
+  drop_food=function(self)
+    food={
+      4,          -- sprite number
+      self.x,     -- x position
+      self.y,     -- y position
+      1,          -- width
+      1,          -- height
+      rnd({true, false}),  -- flip x
+      rnd({true, false})   -- flip y
+    }
+
+    add(foods, food)
+  end,
 }
